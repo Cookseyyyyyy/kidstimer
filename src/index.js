0e30b6e972b862aaa146ae9a5641e1a0f4c359fb
Cooksey <matthew.cooksey@circusstreet.com>
2024-11-24 16:54:13 +0000

    trying to fix scrolling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const setViewportHeight = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+// Initial execution
+setViewportHeight();
+
+// Re-calculate on resize
+window.addEventListener('resize', setViewportHeight);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
